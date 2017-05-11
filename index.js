47964a9c4bc4e0fa342f0e43aafdc4b01546e909
Steve Hirschhorn <hirschhorn970@gmail.com>
2017-05-10 21:39:46 -0600

    working locally

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,10 +8,18 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'html');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/index.html');
 });
 
 app.listen(app.get('port'), function() {
